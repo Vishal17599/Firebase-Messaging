c5b58c9fb2add61a1ad6f8ab11b022b9d7d0459b
Vishal17599 <Vishal.parmar17599@gmail.com>
2020-03-03 22:59:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notification_application/widget/messaging_widget.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String appTitle = 'Firebase messaging';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: appTitle,
+    home: MainPage(appTitle: appTitle),
+  );
+}
+
+class MainPage extends StatelessWidget {
+  final String appTitle;
+
+  const MainPage({this.appTitle});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(appTitle),
+    ),
+    body: MessagingWidget(),
+  );
+}
